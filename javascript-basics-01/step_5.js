23a42f780683bcb30be6fc0b72d0dd7d017ff389
malekkh <malekkhoder98@gmail.com>
2022-12-08 20:15:28 +0200

    step5js1

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,15 @@
+const validateButton = document.querySelector("#validate");
+const firstNumInput = document.querySelector("#first_number");
+const secondNumInput = document.querySelector("#second_number");
+
+let num1 = parseInt(prompt("Please enter the first number"));
+let num2 = parseInt(prompt("Please enter the second number"));
+
+firstNumInput.value = num1;
+secondNumInput.value = num2;
+
+validateButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    alert ("The answer is: " + (num1 * num2));
+    firstNumInput.value = secondNumInput.value = "";
+})
